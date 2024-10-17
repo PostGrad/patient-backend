06c22eac7f2567be81b1c3e106c01dab0cdb7990
PostGrad <pranaypatel008@gmail.com>
2024-10-17 19:08:47 +0530

    add db name

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(bodyParser.json());
 app.use("/api/auth", authRoutes);
 
 mongoose
-  .connect(process.env.MONGODB_URI)
+  .connect(process.env.MONGODB_URI, { dbName: "patient-dashboard" })
   .then(() => console.log("MongoDB connected"))
   .catch((err) => console.error("MongoDB connection error:", err));
 
